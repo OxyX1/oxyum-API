99a2f7c74748d3f7390304b11e60adf3ed5ec8ea
oxyum services <29GariglioSe@fortcherry.org>
2025-02-26 02:09:28 -0500

    push

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-/*
-
-This is the oxyum networks official api.
-
-tools:
-
-cloud browser
-cloud desktop
-chat system
-
-future ideas:
-
-universal frontend server --> its a server were the user can make whatever they want out of it.
-
-*/
-
-
 const express = require('express');
-const http = require('http');
 const path = require('path');
-const cors = require('cors');
-
 const app = express();
-const port = process.env.PORT || 8080;
 
-app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-
-app.get('/cloudbrowser', (req, res) => {
-    res.json({ message: 'Cloud browser endpoint reached successfully!' });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-http.createServer(app).listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
